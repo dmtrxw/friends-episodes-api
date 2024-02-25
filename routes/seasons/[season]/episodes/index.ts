b8d873e00ef9157148a51682b4e76fc82be0dd12
Dimitri <gitmitri@gmail.com>
2024-02-25 12:29:28 +0700

    fix: get episodes by season number
    - Change route structure to /seasons/[season]/episodes
    - Make errors more readable

---
+++ routes/seasons/[season]/episodes/index.ts
@@ -0,0 +1,27 @@
+import { db } from '../../../../lib/db'
+
+export default eventHandler(async event => {
+    try {
+        const season = getRouterParam(event, 'season')
+
+        const episodes = await db
+            .collection('episodes')
+            .find(
+                { season: Number(season) },
+                { projection: { _id: 0 }, sort: { no_in_season: 1 } }
+            )
+            .toArray()
+
+        if (episodes.length === 0) {
+            setResponseStatus(event, 404)
+            return { message: 'Season not found. Check your payload' }
+        }
+
+        return episodes
+    } catch (err) {
+        console.log(err)
+
+        setResponseStatus(event, 500)
+        return { message: 'Internal server error, please try again later.' }
+    }
+})
